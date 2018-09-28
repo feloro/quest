7edcc1a6ab01ce6e6ee38fbc69ec6fec6a96d2da
feloro <istomin@saject.ru>
2018-09-29 01:11:16 +0300

    img settings

--- js/validator.js
+++ js/validator.js
@@ -1,6 +1,10 @@
 var storage = {
-    "1" : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
-    "2" : ""
+    "1" : "79c2e71f476b1425892254db09f69aa40b9ff23d5a713838721e2f70cb2d5809",
+    "2" : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3",
+    "3" : "ERPheTAE5fhKf8hEG8V1W",
+    "ERPheTA": "",
+    "E5fhKf8": "",
+    "hEG8V1W": ""
 };
 
 function validatePassword() {
